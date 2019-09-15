b195ae2cd8414bd8dafd58342ce1c041efcc005c
Sara LaFleur <saralafleur47@gmail.com>
2019-09-15 15:51:27 -0600

    Add project files.

---
+++ PingTest/Ping.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace PingTest
+{
+    public static class InternalPing
+    {
+        public static string Status { get; set; }
+
+        internal static void Start()
+        {
+            LocalPing();
+            Status = "Running";
+            
+        }
+        internal static void Stop()
+        {
+            Status = "Stopped";
+        }
+
+        public static void LocalPing()
+        {
+            // Ping's the local machine.
+            Ping pingSender = new Ping();
+            IPAddress address = IPAddress.Loopback;
+            PingReply reply = pingSender.Send(address);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+            else
+            {
+                Console.WriteLine(reply.Status);
+            }
+        }
+
+        internal static void DisplayStatus()
+        {
+            Console.WriteLine("Status...");
+        }
+    }
+}
---
+++ PingTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingTest
+{
+    class Program
+    {
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Ping Test");
+            Console.WriteLine(string.Format("Status: {0}", InternalPing.Status));
+            Console.WriteLine();
+            Console.WriteLine("1. Start");
+            Console.WriteLine("2. Stop");
+            Console.WriteLine("3. Status");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("");
+            Console.WriteLine("Select a Command and hit Enter...");
+            var result = Console.ReadLine();
+            bool isNumeric = int.TryParse(result, out int number);
+            if (isNumeric)
+                return number;
+            return Convert.ToInt32(result);
+        }
+
+
+        static void Main(string[] args)
+        {
+            InternalPing.Status = "Idle";
+
+            
+
+            int userInput;
+            do
+            {
+
+                userInput = DisplayMenu();
+
+                switch (userInput)
+                {
+                    case 1:
+                        InternalPing.Start();
+                        break;
+                    case 2:
+                        InternalPing.Stop();
+                        break;
+                    case 3:
+                        InternalPing.DisplayStatus();
+                        break;
+                }
+
+            } while (userInput != 4);
+
+        }
+    }
+}
